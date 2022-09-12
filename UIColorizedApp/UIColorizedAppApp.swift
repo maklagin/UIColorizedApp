da1a7d5ae4fd1b0c8cb48979dff3c91bde2c148c
maklagin <108239343+maklagin@users.noreply.github.com>
2022-09-12 17:07:00 +0300

    Initial Commit

---
+++ UIColorizedApp/UIColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIColorizedAppApp.swift
+//  UIColorizedApp
+//
+//  Created by Максим Кулагин on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
